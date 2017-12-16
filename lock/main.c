e572ca156afeb21adb7f233e9c43668a031ca251
qwexfour <deman7866@mail.ru>
2017-12-16 03:37:27 +0300

    Small changes

--- lock/main.c
+++ lock/main.c
@@ -11,9 +11,10 @@ int main( void )
 {
 	//char key_e[255], key_n[255];
 	
-	btoothServer( 1 );
+	tcpServer( PORT_LOCK );
+	//btoothServer( 1 );
 	//requestKey( PORT, IP_SERVER, "Pavlov", "Anton", "Egorovich", key_e, key_n );
 	//printf( "%s %s\n", key_e, key_n );
-	openLock();
+	//openLock();
 	return 0;
 }
